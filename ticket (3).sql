890f4bff43f6d11301e5fc407f7697dff928dc7c
alishamsfi80 <lshms3496@gmail.com>
2023-01-10 15:47:41 -0800

    add sql

--- ticket (3).sql
+++ ticket (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 10, 2023 at 08:48 PM
+-- Generation Time: Jan 10, 2023 at 11:46 PM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -38,7 +38,18 @@ CREATE TABLE IF NOT EXISTS `message` (
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_ticket` (`id_ticket`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id`, `send_message`, `recive_message`, `id_ticket`, `title_ticket`, `status`, `user_id`) VALUES
+(7, 'salam kardam', NULL, 13, 'salam', 0, 7),
+(8, 'salam kardam', NULL, 14, 'aleik', 0, 7),
+(9, 'salam kardam', NULL, 15, 'ss', 0, 8),
+(10, 'salam kardam', NULL, 16, 'tt', 0, 8),
+(11, 'salam kardam', NULL, 17, 'ss', 0, 7);
 
 -- --------------------------------------------------------
 
@@ -53,9 +64,19 @@ CREATE TABLE IF NOT EXISTS `new_ticket` (
   `content` text COLLATE utf8_persian_ci NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `title` (`title`),
-  KEY `user_id` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+  UNIQUE KEY `uq_yourtablename` (`user_id`,`title`)
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `new_ticket`
+--
+
+INSERT INTO `new_ticket` (`id`, `title`, `content`, `user_id`) VALUES
+(13, 'salam', 'salam kardam', 7),
+(14, 'aleik', 'salam kardam', 7),
+(15, 'ss', 'salam kardam', 8),
+(16, 'tt', 'salam kardam', 8),
+(17, 'ss', 'salam kardam', 7);
 
 -- --------------------------------------------------------
 
@@ -74,7 +95,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `is_admin` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `user_name`, `email`, `password`, `is_admin`) VALUES
+(6, 'hedye', 'bahadori', 'hedye', 'hedye@gmail.com', '123456', 1),
+(7, 'ali', 'shams', 'ali', 'ali@gmail.com', '123456', 0),
+(8, 'elahe', 'shams', 'elahe', 'elahe@gmail.com', '123456', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
