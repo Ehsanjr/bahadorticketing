2b833f45b8cc02d2566cae2c6dc07145c5f63396
ehsnjr <ehsanjr1380@gmail.com>
2023-01-11 15:30:24 +0330

    add about

--- index.php
+++ index.php
@@ -12,44 +12,40 @@
                 <hr style="color: white; margin-right:20px; margin-left: 20px;" />
                 <div class="sidebar-body">
                     <ul>
-                        <li class="sidebarli"><a href="index.php" style="color: white; text-decoration: none;">صفحه
+                        <li class="sidebarli"><a href="index.php" class="sidebarlink">صفحه
                                 اصلی</a></li>
-                        <li class="sidebarli"><a>اخبار مقالات</a></li>
-                        <li class="sidebarli"><a>سوالات متداول</a></li>
-                        <li class="sidebarli"><a>درباره ما</a></li>
+
+                        <li class="sidebarli"><a class="sidebarlink" href="about.php">درباره ما</a></li>
                     </ul>
                     <?php
-                    if (isset ($_SESSION['login'])&&$_SESSION['login'] &&!isset ($_SESSION['admin']))
-                    {
+                    if (isset($_SESSION['login']) && $_SESSION['login'] && !isset($_SESSION['admin'])) {
                         ?>
-                    <ul>
-                        <hr style="color: white; margin-right:20px; margin-left: 20px;" />
-                        <li class="sidebarli"><a href="addticket.php">ثبت تیکت جدید</a></li>
-                        <li class="sidebarli"><a href="manageticket.php">مدیریت تیکت ها</a></li>
-                    </ul>
-                    <?php
+                        <ul>
+                            <hr style="color: white; margin-right:20px; margin-left: 20px;" />
+                            <li class="sidebarli"><a class="sidebarlink" href="addticket.php">ثبت تیکت جدید</a></li>
+                            <li class="sidebarli"><a class="sidebarlink" href="manageticket.php">مدیریت تیکت ها</a></li>
+                        </ul>
+                        <?php
                     }
                     ?>
-                   
+
                     <?php
-                    if (isset ($_SESSION['login'])&&$_SESSION['login']&&isset ($_SESSION['admin'])&&$_SESSION['admin'])
-                    {
+                    if (isset($_SESSION['login']) && $_SESSION['login'] && isset($_SESSION['admin']) && $_SESSION['admin']) {
                         ?> <!--  if admin -->
-                    <ul>
-                        <hr style="color: white; margin-right:20px; margin-left: 20px;" />
-                        <li class="sidebarli"><a href="adminmanageticket.php">بررسی تیکت ها</a></li>
-                        <!-- <li class="sidebarli"><a>مدیریت تیکت ها</a></li> -->
-                    </ul>
-                    <?php
+                        <ul>
+                            <hr style="color: white; margin-right:20px; margin-left: 20px;" />
+                            <li class="sidebarli"><a class="sidebarlink" href="adminmanageticket.php">بررسی تیکت ها</a></li>
+                            <!-- <li class="sidebarli"><a>مدیریت تیکت ها</a></li> -->
+                        </ul>
+                        <?php
                     }
                     ?>
-                       <?php
-                    if (isset ($_SESSION['login'])&&$_SESSION['login'])
-                    {
+                    <?php
+                    if (isset($_SESSION['login']) && $_SESSION['login']) {
                         ?>
-                       <ul>
-                       <hr style="color: white; margin-right:20px; margin-left: 20px;" />
-                        <li class="sidebarli"><a href="logout.php">خروج  </a></li>
+                        <ul>
+                            <hr style="color: white; margin-right:20px; margin-left: 20px;" />
+                            <li class="sidebarli"><a class="sidebarlink" href="logout.php">خروج </a></li>
                         </ul>
                         <?php
                     }
